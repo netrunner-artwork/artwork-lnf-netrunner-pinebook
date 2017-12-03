b5c7dfdadbe9057cf3272369f0120061eb180c39
Rohan Garg <rohan@garg.io>
2017-12-03 21:58:23 +0100

    Don't set a position on the calamares desktop file

--- usr/share/plasma/look-and-feel/org.kde.netrunner-pinebook.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ usr/share/plasma/look-and-feel/org.kde.netrunner-pinebook.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -3,7 +3,7 @@ applet.currentConfigGroup = new Array ("Wallpaper", "org.kde.image", "General")
 applet.writeConfig("Image", "file:///usr/share/wallpapers/netrunner-pinebook/Pinebook-Wallpaper-3.jpg")
 applet.writeConfig("FillMode", "2")
 applet.currentConfigGroup = new Array("General");
-applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2,desktop:/calamares.desktop,0,3")
+applet.writeConfig("positions","1,7,desktop:/mycomputer.desktop,0,0,desktop:/Network,0,1,desktop:/welcome.desktop,0,2")
 applet.writeConfig("pressToMove",true);
 applet.writeConfig("showToolbox",false);
 applet.writeConfig("popups",false);
